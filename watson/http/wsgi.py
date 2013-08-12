11c21f0e36ac9043d44ef6b99009d4ec7c028672
Simon Coulton <simon.coulton@gmail.com>
2013-08-12 20:55:48 +1000

    Maintain empty GET values and default to True

--- watson/http/wsgi.py
+++ watson/http/wsgi.py
@@ -19,8 +19,8 @@ def get_form_vars(environ):
     field_storage = FieldStorage(fp=environ['wsgi.input'], environ=environ,
                                  keep_blank_values=True)
     get = MultiDict()
-    for name, value in parse_qsl(environ.get('QUERY_STRING')):
-        get[name] = value
+    for name, value in parse_qsl(environ.get('QUERY_STRING'), keep_blank_values=True):
+        get[name] = value if value else True
     return _process_field_storage(field_storage, get=get)
 
 
